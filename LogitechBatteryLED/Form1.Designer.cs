d9f4d204d9b822b95ef2a12c0ece8e771768f4eb
QuiNz0r <quink89@gmail.com>
2020-08-22 21:06:28 +0200

    Add project files.

---
+++ LogitechBatteryLED/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace LogitechBatteryLED
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.AlphaMode = new System.Windows.Forms.ToolStripMenuItem();
+            this.Exit = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 39);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(220, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Logitech Battery LED";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AlphaMode,
+            this.Exit});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 70);
+            // 
+            // AlphaMode
+            // 
+            this.AlphaMode.Checked = true;
+            this.AlphaMode.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.AlphaMode.Name = "AlphaMode";
+            this.AlphaMode.Size = new System.Drawing.Size(180, 22);
+            this.AlphaMode.Text = "AlphaMode: ON";
+            this.AlphaMode.Click += new System.EventHandler(this.AlphaMode_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(180, 22);
+            this.Exit.Text = "Exit";
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 268);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem AlphaMode;
+        private System.Windows.Forms.ToolStripMenuItem Exit;
+    }
+}
+
---
+++ LogitechBatteryLED/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using Memory;
+using LedCSharp;
+using System.Threading;
+
+namespace LogitechBatteryLED
+{
+    public partial class Form1 : Form
+    {
+        private Mem _memory = new Mem();
+        private BackgroundWorker _backgroundWorker = new BackgroundWorker();
+        private int _checkIntervalms = 20000;
+        private bool _alphaMode = true;
+        private bool _logiSDKInit = false;
+        private float _batteryCharge = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _backgroundWorker.DoWork += new DoWorkEventHandler(backgroundWorker1_DoWork);
+            _backgroundWorker.RunWorkerAsync();
+        }
+
+        void InitializeLogiSDK()
+        {
+            _logiSDKInit = LogitechGSDK.LogiLedInitWithName("Battery LED");
+
+            if (_logiSDKInit) Console.WriteLine("LED SDK Initialized");
+            LogitechGSDK.LogiLedSetTargetDevice(LogitechGSDK.LOGI_DEVICETYPE_RGB);
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            while (true)
+            {
+                if (!_logiSDKInit)
+                {
+                    LogitechGSDK.LogiLedShutdown();
+                    int pid = _memory.GetProcIdFromName("lghub_agent");
+                    if (pid <= 0)
+                    {
+                        Console.WriteLine("looking for lghub...");
+                        Thread.Sleep(5000);
+                        continue;
+                    }
+                    else
+                    {
+                        InitializeLogiSDK();
+                    }
+                }
+
+
+
+                Console.WriteLine("reading memory...");
+                ReadBatteryStatus();
+                SetColor(_batteryCharge);
+                Thread.Sleep(_checkIntervalms);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            LogitechGSDK.LogiLedShutdown();
+        }
+
+        void SetColor(float batteryPercentage)
+        {
+            float Alpha = (batteryPercentage + 5) / 100; // 5 = offset alpha
+
+            float Rlower = 100;
+            float Glower = batteryPercentage * 2;
+
+            float Rupper = (100 - batteryPercentage) * 2;
+            float Gupper = 100;
+
+            if (_alphaMode)
+            {
+                Rlower *= Alpha;
+                Glower *= Alpha;
+                Rupper *= Alpha;
+                Gupper *= Alpha;
+            }
+
+            if (batteryPercentage > 0)
+            {
+                //if (batteryPercentage <= 10)
+                //{
+                //    LogitechGSDK.LogiLedPulseLighting(100, (int)batteryPercentage, 0, _checkIntervalms, 500);
+                //}
+                if (batteryPercentage <= 50)
+                {
+                    LogitechGSDK.LogiLedSetLightingForTargetZone(DeviceType.Mouse, 1, (int)Rlower, (int)Glower, 0);
+                    Console.WriteLine("RGB: " + (int)batteryPercentage + "% " + (int)Rlower + " " + (int)Glower + " 0");
+                }
+                else if (batteryPercentage > 50)
+                {
+                    LogitechGSDK.LogiLedSetLightingForTargetZone(DeviceType.Mouse, 1, (int)Rupper, (int)Gupper, 0);
+                    Console.WriteLine("RGB: " + (int)batteryPercentage + "% " + (int)Rupper + " " + (int)Gupper + " 0");
+                }
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            SetColor(trackBar1.Value);
+        }
+
+        private void ReadBatteryStatus()
+        {
+            int pid = _memory.GetProcIdFromName("lghub_agent");
+            bool openProc = false;
+            if (pid > 0) openProc = _memory.OpenProcess(pid);
+
+            if (openProc)
+            {
+                _batteryCharge = _memory.ReadFloat("lghub_agent.exe+0x015C5048,0x10,0x0,0x30,0x58,0x2E8,0x0,0x1B8,0x28,0x170,0x0,0x450,0x30,0x30,0x90,0x348");
+                notifyIcon1.Text = "Logitech Battery LED " + (int)_batteryCharge + "%";
+            }
+            else
+            {
+                _logiSDKInit = false;
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+            notifyIcon1.Visible = true;
+            Hide();
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void AlphaMode_Click(object sender, EventArgs e)
+        {
+            _alphaMode = !_alphaMode;
+            AlphaMode.Checked = _alphaMode;
+            AlphaMode.Text = "AlphaMode: " + (_alphaMode ? "ON" : "OFF");
+        }
+
+    }
+}
