39954b37c12461024d0d4e0870a6b8a0b5bf1137
QuiNz0r <quink89@gmail.com>
2020-08-23 00:00:11 +0200

    make app only open one instance at a time

--- LogitechBatteryLED/Program.cs
+++ LogitechBatteryLED/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,12 +9,25 @@ namespace LogitechBatteryLED
 {
     static class Program
     {
+        private static Mutex _mutex = null;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            const string appName = "LogitechBatteryLED";
+            bool createdNew;
+
+            _mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                //app is already running! Exiting the application  
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
